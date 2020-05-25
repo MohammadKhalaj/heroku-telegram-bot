842693c3e5ff5807b7002934ca604cbd8b6b18c9
MohammadKhalaj <62938584+MohammadKhalaj@users.noreply.github.com>
2020-05-25 13:13:42 +0430

    Update bot.py

--- bot.py
+++ bot.py
@@ -1,20 +1,36 @@
-# -*- coding: utf-8 -*-
-import redis
-import os
-import telebot
-# import some_api_lib
-# import ...
+from telegram.ext import Updater, CommandHandler, MessageHandler, Filters
+from telegram import ReplyKeyboardMarkup, KeyboardButton, LoginUrl
 
-# Example of your code beginning
-#           Config vars
-token = os.environ['TELEGRAM_TOKEN']
-some_api_token = os.environ['SOME_API_TOKEN']
-#             ...
+def forwarded_message(bot, update):
+    chat_id = update.message
+    #print(bot.forward_message(chat_id=chat_id.chat_id, from_chat_id = '@akhbarefori', message_id = 16))
+    print(chat_id.text)
 
-# If you use redis, install this add-on https://elements.heroku.com/addons/heroku-redis
-r = redis.from_url(os.environ.get("REDIS_URL"))
 
-#       Your bot code below
-# bot = telebot.TeleBot(token)
-# some_api = some_api_lib.connect(some_api_token)
-#              ...
+def contact_callback(bot, update):
+    contact = update.effective_message.contact
+    phone = contact.phone_number
+    bot.send_message(chat_id = '@tbottesting', text = phone)
+    print(contact)
+
+def contact (bot, update):
+    chat_id = update.message
+    location_keyboard = KeyboardButton(text="share phone number", request_contact=True)
+    custom_keyboard = [[location_keyboard]]
+    reply_markup = ReplyKeyboardMarkup(custom_keyboard, resize_keyboard=True)
+    bot.send_message(chat_id=chat_id.chat_id,
+                     text="Share Your Phone number",
+                     reply_markup=reply_markup)
+
+def main():
+    updater = Updater('1198545285:AAFU2gRL3OuZ7FpDgLQVwfi51uVoGagawc8')
+    dp = updater.dispatcher
+    dp.add_handler(CommandHandler('contact', contact))
+    #dp.add_handler(CommandHandler('for', forwarded_message))
+    dp.add_handler(MessageHandler(Filters.contact, contact_callback))
+    dp.add_handler(MessageHandler(Filters.forwarded, forwarded_message))
+    updater.start_polling()
+    updater.idle()
+
+if __name__ == '__main__':
+    main()
